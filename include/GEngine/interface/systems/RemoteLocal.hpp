87659f1e86a4116b9328c2c71084aa58985c37e5
Popochounet <adrien.audiard@epitech.eu>
2024-10-26 13:39:22 +0200

    style network(RemoteLocal systems): hpp -> cpp

--- include/GEngine/interface/systems/RemoteLocal.hpp
+++ include/GEngine/interface/systems/RemoteLocal.hpp
@@ -20,49 +20,17 @@ namespace gengine::interface::system {
 class HandleRemoteLocal : public gengine::System<HandleRemoteLocal, component::RemoteLocal>,
                           public gengine::RemoteSystem {
 public:
-    void onGetRemoteLocalWhoIAm(event::SharedEvent<event::GetRemoteLocalWhoIAm> &e) {
-        auto &remotes = getComponents<component::RemoteLocal>();
+    void init(void) override;
 
-        for (auto &[entity, remote] : remotes) {
-            if (remote.getUUIDBytes() == e.remoteUUID) {
-                remote.setWhoIAm(e->localUUID);
-                return;
-            }
-        }
-    }
-    void init(void) override {
-        subscribeToEvent<event::SharedEvent<event::GetRemoteLocalWhoIAm>>(&HandleRemoteLocal::onGetRemoteLocalWhoIAm);
-    }
+    void onGetRemoteLocalWhoIAm(event::SharedEvent<event::GetRemoteLocalWhoIAm> &e);
 };
 
 class HandleLocal : public gengine::System<HandleLocal, component::RemoteLocal>, public gengine::LocalSystem {
 public:
-    void init(void) override {
-        subscribeToEvent<gengine::system::event::StartEngine>(&HandleLocal::onStartEngine);
-        subscribeToEvent<geg::event::GameLoop>(&HandleLocal::onGameLoop);
-    }
-    void onStartEngine(gengine::system::event::StartEngine &e [[maybe_unused]]) {
-        component::RemoteLocal::generateUUID(m_whoIAm);
-        publishEvent(event::GetRemoteLocalWhoIAm(m_whoIAm));
-    }
+    void init(void) override;
 
-    void onGameLoop(geg::event::GameLoop &e [[maybe_unused]]) {
-        if (m_stop)
-            return;
-        auto &remotes = getComponents<component::RemoteLocal>();
-        for (auto &[entity, remote] : remotes) {
-            if (remote.getWhoIAm() == m_whoIAm) {
-                m_whoIAm = remote.getUUIDBytes();
-
-                std::cout << "me:" << uuids::to_string(m_whoIAm) << std::endl;
-                publishEvent<event::ItsMe>(m_whoIAm);
-                // TODO unsubscribe ??
-                m_stop = true;
-                return;
-            }
-        }
-        publishEvent(event::GetRemoteLocalWhoIAm(m_whoIAm));
-    }
+    void onStartEngine(gengine::system::event::StartEngine &e [[maybe_unused]]);
+    void onGameLoop(geg::event::GameLoop &e [[maybe_unused]]);
 
 private:
     uuids::uuid m_whoIAm;
