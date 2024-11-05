cc464bc8f47b17a5ed382b4ea5dd219d102d8190
b3W <basile.fouquet@epitech.eu>
2024-11-06 00:47:46 +0100

    added GEngine: exit stop binari
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

---
+++ rtype-3/include/components/Background.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-LYN-5-1-rtype-basile.fouquet
+** File description:
+** Background.hpp
+*/
+
+#pragma once
+
+#include "GEngine/libdev/Component.hpp"
+
+namespace rtype::component {
+struct Background : public gengine::Component<Background> {
+    Background() {
+    }
+};
+} // namespace rtype::component
---
+++ rtype-3/include/components/Player.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-LYN-5-1-rtype-basile.fouquet
+** File description:
+** Player.hpp
+*/
+
+#pragma once
+
+#include "GEngine/libdev/Component.hpp"
+
+#define DEFAULT_SPEED 5
+#define DEFAULT_LIFES 3
+
+namespace rtype::component {
+struct Player : public gengine::Component<Player> {
+    float speed;
+    int lifes;
+    bool ready = false;
+
+    Player(float newSpeed = DEFAULT_SPEED, int lifes = DEFAULT_LIFES)
+        : speed(newSpeed)
+        , lifes(lifes) {
+    }
+
+    bool operator==(const Player &) const = default;
+};
+} // namespace rtype::component
