7fd451ede0c825bb5662c0490df368123e47041a
thomas-pommier-epi <thomas.pommier@epitech.eu>
2024-11-05 09:43:58 +0100

    fixed all: beautify on isystem and net_wait files

--- include/ecs/system/IsSystem.hpp
+++ include/ecs/system/IsSystem.hpp
@@ -9,8 +9,8 @@
 
 namespace ecs::system {
 class IsSystem {
-    public:
-        IsSystem() = default;
-        virtual ~IsSystem() = default;
+public:
+    IsSystem() = default;
+    virtual ~IsSystem() = default;
 };
 } // namespace ecs::system
