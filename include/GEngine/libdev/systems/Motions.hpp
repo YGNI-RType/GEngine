b275b30d63876aa84d6f117e27e33a08c0b7e66d
Popochounet <adrien.audiard@epitech.eu>
2024-11-06 01:40:41 +0100

    fix libdev: compilation

--- include/GEngine/libdev/systems/Motions.hpp
+++ include/GEngine/libdev/systems/Motions.hpp
@@ -25,7 +25,7 @@ namespace gengine::system {
 class Motion2D : public System<Motion2D, component::Transform2D, component::Velocity2D> {
 public:
     void init(void);
-    void onGameLoop(event::RenderLoop &);
+    void onGameLoop(event::GameLoop &);
 };
 
 class Motion3D : public System<Motion3D, component::Transform3D, component::Velocity3D> {
