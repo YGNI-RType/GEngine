d526ee1c6533193821c228842b66d05536558ee3
Arcod7 <antoine.esman@epitech.eu>
2024-11-06 01:27:52 +0100

    fix porphunt: windows compilation

--- source/GEngine/libdev/systems/driver/output/Draw.cpp
+++ source/GEngine/libdev/systems/driver/output/Draw.cpp
@@ -142,11 +142,8 @@ void DrawModel::onStartEngine(gengine::system::event::StartEngine &e) {
     float ambiantValue[4] = {0.1f, 0.1f, 0.1f, 1.0f};
     SetShaderValue(shader, ambientLoc, ambiantValue, SHADER_UNIFORM_VEC4);
 
-    // Vector3 lightPosRed = {1.16, 1.40, 4.76};
-    lights[0] = CreateLight(LIGHT_POINT, (Vector3){1.16, 1.40, 4.76}, (Vector3){0, 0, 0}, RED, shader);
-
-    // Vector3 lightPosBlue = {6.8, 1.4, 4.5};
-    lights[1] = CreateLight(LIGHT_POINT, (Vector3){6.8, 1.4, 4.5}, (Vector3){0, 0, 0}, BLUE, shader);
+    lights[0] = CreateLight(LIGHT_POINT, {1.16, 1.40, 4.76}, {0, 0, 0}, RED, shader);
+    lights[1] = CreateLight(LIGHT_POINT, {6.8, 1.4, 4.5}, {0, 0, 0}, BLUE, shader);
 }
 
 void DrawModel::onStopEngine(gengine::system::event::StopEngine &e) {
